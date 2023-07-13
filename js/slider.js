ef80381569e719a0d1cdf55106762211043de88a
RapaN <kostyasteamonly@gmail.com>
2023-07-13 21:02:48 +0300

    add buttons for slider on 1410px, fix some problems with slider

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,10 @@ $(".CS-comments-block").slick({
   slidesToShow: 2,
   slidesToScroll: 1,
   arrows: true,
+  appendArrows: $(".CS-slider-buttons"),
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>',
+
   responsive: [
     {
       breakpoint: 768,
